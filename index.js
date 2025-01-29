70924edfef0804c82073b8d5bdfba16ca1d09058
ArmaineCervantes <125836706+ArmaineCervantes@users.noreply.github.com>
2025-01-29 11:28:10 +0800

    change port

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http = require("http");
-const port = 3001;
+const port = 3000;
 
 const server = http.createServer(function(req, res) {
-    res.write("Hellooo");
+    res.write("Hello!");
     res.end();
 });
 
 server.listen(port, (err) => {
     if (err) {
-        console.log(`Unable to connect to port ${port}`);
+        console.log(`Unable to listen to port ${port}`);
     } else {
         console.log(`Server listening on port ${port}`);
     }
