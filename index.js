b9da0400509238f6933213ee6f24106bc1901275
ArmaineCervantes <125836706+ArmaineCervantes@users.noreply.github.com>
2025-01-29 11:39:16 +0800

    update text content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = 3000;
 
 const server = http.createServer(function(req, res) {
-    res.write("Hello!");
+    res.write("Hello! This is a sample text");
     res.end();
 });
 
