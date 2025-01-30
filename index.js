408b6634ed0e6887819b13ab6b1ea34b361deddb
ArmaineCervantes <125836706+ArmaineCervantes@users.noreply.github.com>
2025-01-31 06:24:56 +0800

    add display type of

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ async function restart() {
                 // exec("sudo systemctl restart tomcat", (error, stdout, stderr) => {
                 exec("sudo systemctl restart tomcat", (error, stdout, stderr) => {
                     console.log("error: ", error);
+                    console.log("error: ", typeof error);
                     console.log("stdout: ", stdout);
                     console.log("stderr: ", stderr);
-                    
+
                     console.log("Restart tomcat success:", formatDate, stdout, error, stderr);
                 });
             } else {
