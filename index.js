d71b3ee559d1bc85f5cae5d015be1025b6baca68
ArmaineCervantes <125836706+ArmaineCervantes@users.noreply.github.com>
2025-01-31 07:46:40 +0800

    change url for testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process");
 async function restart() {
     while (true) {
         // This will wait until the response is given, might take a few seconds or a minute if response is 504
-        await axios.get("https://aasdfsdfdsfdsfsdbode.com/")
+        await axios.get("https://aasdfsdfsdfbode.com/")
         .then(response => {
             // Site is working
             console.log("Site is working..");
@@ -40,7 +40,7 @@ async function restart() {
             }
         });
 
-        await new Promise(resolve => setTimeout(resolve, 10000)); // 300000 = 5 minutes // 10000 = 10 seconds
+        await new Promise(resolve => setTimeout(resolve, 300000)); // 300000 = 5 minutes // 10000 = 10 seconds
     }
 }
 
