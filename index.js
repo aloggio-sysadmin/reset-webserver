0ce4619bca393845f1ea395a4a8de410f9b08fa1
ArmaineCervantes <125836706+ArmaineCervantes@users.noreply.github.com>
2025-01-30 14:04:39 +0800

    add while loop

--- index.js
+++ index.js
@@ -8,39 +8,40 @@ const server = http.createServer(function(req, res) {
 });
 
 async function restart() {
-    // This will wait until the response is given, might take a few seconds or a minute if response is 504
-    await axios.get("https://asdasdfasdf.com/")
-    .then(response => {
-        // Site is working
-        console.log("Site is working..");
-    })
-    .catch(async err => {
-        let error = err;
-        error.status = 504;
-        
-        if (error.status && error.status == 504) {
-            const date = new Date(Date.now());
-            const formatDate = date.toLocaleString("en-AU", {
-                weekday: "long",
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                hour12: true
-            });
-            console.log("Reset");
+    while (true) {
+        // This will wait until the response is given, might take a few seconds or a minute if response is 504
+        await axios.get("https://asdasdfasdf.com/")
+        .then(response => {
+            // Site is working
+            console.log("Site is working..");
+        })
+        .catch(async err => {
+            let error = err;
+            error.status = 504;
             
-            // exec("ls", (error, stdout, stderr) => {
-            //     console.log("Restart tomcat success:", formatDate, stdout, error, stderr);
-            // });
-        } else {
-            console.log("error: ", error);
-        }
-    });
+            if (error.status && error.status == 504) {
+                const date = new Date(Date.now());
+                const formatDate = date.toLocaleString("en-AU", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    hour12: true
+                });
+                
+                exec("sudo systemctl restart tomcat", (error, stdout, stderr) => {
+                    console.log("Restart tomcat success:", formatDate, stdout, error, stderr);
+                });
+            } else {
+                console.log("error: ", error);
+            }
+        });
 
-    await new Promise(resolve => setTimeout(resolve, 5000)); // 30000 = 30 sec, 3600000 = 1 hour
+        await new Promise(resolve => setTimeout(resolve, 5000)); // 30000 = 30 sec, 3600000 = 1 hour
+    }
 }
 
 server.listen(port, (err) => {
@@ -49,7 +50,6 @@ server.listen(port, (err) => {
         process.exit(1);
     } else {
         console.log(`Server listening on port ${port}`);
+        restart();
     }
 });
-
-restart();
